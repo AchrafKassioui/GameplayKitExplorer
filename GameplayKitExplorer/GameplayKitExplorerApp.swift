bae699c9d1d75a2ba5a3b047580d4ed5bbfcfd75
Achraf Kassioui <achrafkassioui@gmail.com>
2024-10-24 06:29:35 +0100

    Initial Commit

---
+++ GameplayKitExplorer/GameplayKitExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameplayKitExplorerApp.swift
+//  GameplayKitExplorer
+//
+//  Created by Achraf Kassioui on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GameplayKitExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
